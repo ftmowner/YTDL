61fd5f2dfb3182518e445f92941dc5e90b8273c5
FTM DEVELOPER <106005123+ftmdeveloperz@users.noreply.github.com>
2025-04-03 16:57:13 +0530

    Update info.py

--- info.py
+++ info.py
@@ -3,13 +3,13 @@ import os
 SESSION = "ftmbotzx"
 API_ID = int(os.getenv("API_ID", 22141398))
 API_HASH = os.getenv("API_HASH", '0c8f8bd171e05e42d6f6e5a6f4305389')
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8105194942:AAFzL74g4y3EMJdouoVUtRig4SP_1eZk_xs")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8105194942:AAHisU5gxAc77O5qKoAZluI86w10s9MBV5o")
 LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-1002613994353"))
 DUMP_CHANNEL = int(os.getenv("DUMP_CHANNEL", "-1002613994353"))
 PORT = int(os.getenv("PORT", "8080"))
 FORCE_CHANNEL = int(os.getenv("FORCE_CHANNEL", "-1002087228619"))
 HTTP_PROXY = ''
-MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://ftm:ftm@cluster0.xotfi .mongodb.net/?retryWrites=true&w= majority&appName=Cluster0")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://ftm:ftm@cluster0.xotfi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 MONGO_NAME = os.getenv("MONGO_NAME", "Cluster0")
 ADMINS = [7744665378]
 DAILY_LIMITS = 10 
